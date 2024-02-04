44c40e90324f73ac820237e12b716338adbe1532
tivonlaine <155037950+tivonlaine@users.noreply.github.com>
2024-02-03 16:41:49 -0800

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,58 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+choice=input("What do you choose? Type 0 for Rock, 1 for Paper, and 2 for Scissors.\n")
+choice1=int(choice)
+if choice1==0:
+  print(rock)
+elif choice1==1:
+  print(paper)
+elif choice1==2:
+  print(scissors)
+print("Computer chose:\n")
+choice2=random.randint(0,2)
+if choice2==0:
+  print(rock)
+elif choice2==1:
+  print(paper)
+elif choice2==2:
+  print(scissors)
+if choice1>choice2:
+  if choice1==2 and choice2==0:
+    print("You lose")
+  else:
+    print("You win")
+elif choice1<choice2:
+  if choice1==0 and choice2==2:
+    print("You win")
+  else:
+    print("You lose")
+elif choice1==choice2:
+  print("It's a draw")
+  
